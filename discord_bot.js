1f82968ba145e7959f324caf766068c83a2d695f
Anthamation <Anthamation@gmail.com>
2018-09-06 20:23:02 -0500

    Added new commands and preventions

--- discord_bot.js
+++ discord_bot.js
@@ -1,8 +1,10 @@
 const discord = require('discord.js')
 const client = new discord.Client();
 const config = require("./config.json");
+const hruf_List = require("./HRUF.json");
 
 client.login(config.token);
+
 
 client.on("ready", ()=>{
         console.log("I am ready!");
@@ -10,7 +12,19 @@ client.on("ready", ()=>{
 
 
 client.on("message", (msg) => {
-    if (msg.content.startsWith("ping")) {
-        msg.channel.send("pong!");
+    //Prevents botception
+        if (msg.author.bot) return
+        //ping
+        if (msg.content.startsWith(config.prefix + "ping")) {
+        msg.reply("pong!");
 }
-});
+        //hello
+        if (msg.content.startsWith(config.prefix + "Hello!")){
+        msg.reply("Hi there!");
+}
+        //how are you?
+        if (msg.content.startsWith(config.prefix + 'How are you?')){
+            msg.reply(hruf_List.HRUFList[Math.floor(Math.random() * 10)])
+        }
+    }   
+);
